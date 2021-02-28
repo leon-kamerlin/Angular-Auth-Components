ed500e404051ea54ad3641d50c6d00c8103d6861
leonkamerlinn <leonkamerlin@MacBook-Pro.local>
2021-03-01 00:11:50 +0100

    works

--- projects/auth-components-example/src/app/app.component.ts
+++ projects/auth-components-example/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslationLoaderService } from 'leon-angular-utils';
+import { LoginData } from 'auth-components';
+import { locale as eng } from './../../i18n/en';
+import { locale as cro } from './../../i18n/hr';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +12,18 @@ import { TranslationLoaderService } from 'leon-angular-utils';
 })
 export class AppComponent {
   constructor(private translate: TranslateService, private translationLoaderService: TranslationLoaderService) {
+    translationLoaderService.loadTranslations(eng, cro);
+    translate.addLangs(['en', 'hr']);
+    translate.setDefaultLang('en');
 
+    const browserLang: string = translate.getBrowserLang();
+    const cacheLang = localStorage.getItem('lang');
+    const lang = cacheLang || browserLang;
+    translate.use(lang.match(/en|hr/) ? lang : 'en');
   }
   title = 'auth-components-example';
+
+  onLogIn(data: LoginData) {
+
+  }
 }
--- projects/auth-components-example/src/app/app.module.ts
+++ projects/auth-components-example/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AuthComponentsModule } from 'auth-components';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -13,8 +15,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AuthComponentsModule,
     BrowserAnimationsModule,
+    TranslateModule,
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
